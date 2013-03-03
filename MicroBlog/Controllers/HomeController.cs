3ca58a2486365632efcbd5824c340d2ae41edf7d
Rockky <devil_rules_universe@yahoo.co.in>
2013-03-03 20:36:29 +0530

    Source Code

---
+++ MicroBlog/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MicroLib.DataViewModel;
+
+namespace MicroBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        MicroOperations.MicroOperations _MicroOperations = new MicroOperations.MicroOperations();
+        RssFeeds _MainFeedData = new RssFeeds();
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            _MainFeedData = _MicroOperations.GetMainFeeds();
+            return View(_MainFeedData);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
